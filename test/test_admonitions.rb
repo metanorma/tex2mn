f85eacab11a7dce5c9a3a75becd2f50358119cc0
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-09-22 10:49:06 +0200

    feat: #29 write tests for existing features

---
+++ test/test_admonitions.rb
@@ -0,0 +1,163 @@
+require "minitest/autorun"
+require "helper"
+
+class TestAdmonitions < Minitest::Test
+  def test_note
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{note}
+          This is a note.
+        \end{note}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [NOTE]
+      ====
+      This is a note.
+      ====
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_tip
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{tip}
+          This is a tip.
+        \end{tip}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [TIP]
+      ====
+      This is a tip.
+      ====
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_important
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{important}
+          This is important.
+        \end{important}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [IMPORTANT]
+      ====
+      This is important.
+      ====
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_caution
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{caution}
+          This is a cautionary advice.
+        \end{caution}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [CAUTION]
+      ====
+      This is a cautionary advice.
+      ====
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_warning
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{warning}
+          This is a warning.
+        \end{warning}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [WARNING]
+      ====
+      This is a warning.
+      ====
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_long_note
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{note}
+          This is a note.
+
+          It contains multiple paragraphs.
+
+          It is pretty long.
+        \end{note}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [NOTE]
+      ====
+      This is a note.
+
+      It contains multiple paragraphs.
+
+      It is pretty long.
+      ====
+
+      Following paragraph.
+    OUTPUT
+  end
+end
+
---
+++ test/test_paragraph_alignment.rb
@@ -0,0 +1,93 @@
+require "minitest/autorun"
+require "helper"
+
+class TestParagraphAlignment < Minitest::Test
+  def test_justified
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        This paragraph is justified.
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      This paragraph is justified.
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_left_aligned
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{flushleft}
+          This paragraph is left aligned.
+        \end{flushleft}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [align=left]
+      This paragraph is left aligned.
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_centered
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{center}
+          This paragraph is centered.
+        \end{center}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [align=center]
+      This paragraph is centered.
+
+      Following paragraph.
+    OUTPUT
+  end
+
+  def test_right_aligned
+    assert_equal render_string(<<~'INPUT'), <<~OUTPUT
+      \documentclass{metanorma}
+      \begin{document}
+        Previous paragraph.
+
+        \begin{flushright}
+          This paragraph is right aligned.
+        \end{flushright}
+
+        Following paragraph.
+      \end{document}
+    INPUT
+
+      Previous paragraph.
+
+      [align=right]
+      This paragraph is right aligned.
+
+      Following paragraph.
+    OUTPUT
+  end
+end
