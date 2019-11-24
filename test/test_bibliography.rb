0bbb0271ca9d7ec7b0ab705048a8598180d54838
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-11-24 16:29:38 +0100

    feat: #8 add basic suport for BibTex

--- test/test_bibliography.rb
+++ test/test_bibliography.rb
@@ -4,6 +4,42 @@ require "minitest/autorun"
 require "helper"
 
 class TestBibliography < Minitest::Test
+  def test_bibtex
+    assert_equal <<~'OUTPUT', render_string(<<~'INPUT')
+      As seen in <<einstein>>, this is cool stuff.
+      Other references are <<latexcompanion>> and <<knuthwebsite>>.
+
+      [bibliography]
+      == References
+
+      * [[[einstein,1]]]
+      {blank}A. Einstein (1905)
+      Zur Elektrodynamik bewegter Körper. (German) [On the electrodynamics of moving bodies].
+      Annalen der Physik322 (10),  pp. 891–921.
+      External Links: http://dx.doi.org/http%3A//dx.doi.org/10.1002/andp.19053221004[Document]
+
+      * [[[latexcompanion,2]]]
+      {blank}M. Goossens, F. Mittelbach, and A. Samarin (1993)
+      The latex companion.
+      Addison-Wesley, Reading, Massachusetts.
+
+      * [[[knuthwebsite,3]]]
+      {blank}D. Knuth
+      Knuth: computers and typesetting.
+      External Links: http://www-cs-faculty.stanford.edu/\~uno/abcde.html[Link]
+
+    OUTPUT
+      \documentclass{Metanorma}
+
+      \begin{document}
+        As seen in \cite{einstein}, this is cool stuff.
+        Other references are \cite{latexcompanion} and \cite{knuthwebsite}.
+
+        \bibliography{test/fixtures/sample}
+      \end{document}
+    INPUT
+  end
+
   def test_embedded
     assert_equal <<~'OUTPUT', render_string(<<~'INPUT')
       This document is an example of thebibliography environment using
@@ -45,22 +81,22 @@ class TestBibliography < Minitest::Test
 
         \begin{thebibliography}{3}
           \bibitem[ISO 7301]{ISO7301}
-            Rice -- Specification
+          Rice -- Specification
 
           \bibitem{latexcompanion}
-            Michel Goossens, Frank Mittelbach, and Alexander Samarin.
-            \textit{The \LaTeX\ Companion}.
-            Addison-Wesley, Reading, Massachusetts, 1993.
+          Michel Goossens, Frank Mittelbach, and Alexander Samarin.
+          \textit{The \LaTeX\ Companion}.
+          Addison-Wesley, Reading, Massachusetts, 1993.
 
           \bibitem{einstein}
-            Albert Einstein.
-            \textit{Zur Elektrodynamik bewegter K{\"o}rper}. (German)
-            [\textit{On the electrodynamics of moving bodies}].
-            Annalen der Physik, 322(10):891–921, 1905.
+          Albert Einstein.
+          \textit{Zur Elektrodynamik bewegter K{\"o}rper}. (German)
+          [\textit{On the electrodynamics of moving bodies}].
+          Annalen der Physik, 322(10):891–921, 1905.
 
           \bibitem{knuthwebsite}
-            Knuth: Computers and Typesetting,
-            \\\texttt{http://www-cs-faculty.stanford.edu/\~{}uno/abcde.html}
+          Knuth: Computers and Typesetting,
+          \\\texttt{http://www-cs-faculty.stanford.edu/\~{}uno/abcde.html}
         \end{thebibliography}
       \end{document}
     INPUT
